c2a9ebe36b3d5b4b5b9584bb0cb464d3358f9184
codefactor-io <support@codefactor.io>
2022-12-02 18:30:38 +0000

    [CodeFactor] Apply fixes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React, { useCallback, useMemo, useState } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useCallback, useMemo, useState} from 'react';
 import RootStack from 'navigation';
-import { Provider } from 'react-redux';
-import { StatusBar, View } from 'react-native';
+import {Provider} from 'react-redux';
+import {StatusBar, View} from 'react-native';
 import Theme from 'style/Theme';
-import { store } from 'redux/reducer';
-import { Colors } from "configs"
+import {store} from 'redux/reducer';
+import {Colors} from 'configs';
 
 export default function App() {
   return (
-    <View style={[Theme.flexOne, { backgroundColor: Colors.White }]}>
+    <View style={[Theme.flexOne, {backgroundColor: Colors.White}]}>
       <Provider store={store}>
         <StatusBar barStyle={'dark-content'} />
         <NavigationContainer>
-            <RootStack />
+          <RootStack />
         </NavigationContainer>
       </Provider>
     </View>
