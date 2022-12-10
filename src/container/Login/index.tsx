49d2f1749cf0695a3249d95a4f1b8fd304b81185
Belgin <61349423+ibelgin@users.noreply.github.com>
2022-12-11 01:27:50 +0530

    Added Text Element and Animation Support Libraries

--- src/container/Login/index.tsx
+++ src/container/Login/index.tsx
@@ -1,29 +1,38 @@
-import React, {memo, useEffect} from 'react';
+import React, {memo, useEffect, useRef} from 'react';
 import {StyleSheet, View} from 'react-native';
 import Container from 'layout/Container';
 import ButtonText from 'components/ButtonText';
-import {Colors} from 'configs';
+import {Colors, Constants} from 'configs';
 import Strings from './messages.en';
-import {initialConfig, signIn} from 'utils/google-signin';
+import {initialConfig, signIn, onSignIn} from 'utils/google-signin';
+import Text from 'components/Text';
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Lottie from 'lottie-react-native';
 
 interface LoginProps {}
 
 const Login = memo((_props: LoginProps) => {
+  const animationRef = useRef<Lottie>(null);
+
   useEffect(() => {
     initialConfig();
+    animationRef.current?.play(30, 120);
   });
 
   return (
-    <Container>
-      <View style={styles.illustration_view} />
+    <Container style={styles.container}>
+      <View style={styles.illustration_view}>
+        <Lottie ref={animationRef} source={require('assets/floating.json')} />
+      </View>
+      <Text paddingTop={90} paddingHorizontal={20} heading>
+        {Strings.WELCOME}
+      </Text>
+      <Text padding={20} description>
+        {Strings.DESC}
+      </Text>
       <ButtonText
-        onPress={() =>
-          signIn().then(val => {
-            console.log(val);
-          })
-        }
+        onPress={() => signIn().then(onSignIn)}
         icon={
           <AntDesign name={Strings.ICON_NAME} size={24} color={Colors.White} />
         }
@@ -34,7 +43,16 @@ const Login = memo((_props: LoginProps) => {
 });
 
 const styles = StyleSheet.create({
-  illustration_view: {},
+  container: {
+    flex: 1,
+    paddingBottom: 10,
+    paddingTop: 10,
+    backgroundColor: Colors.White,
+  },
+  illustration_view: {
+    height: Constants.height / 2,
+    width: Constants.width,
+  },
 });
 
 export default Login;
