5bf947aecd93357a429ffdf32741ed170c3f88cd
Belgin <Belgin.confidential@gmail.com>
2023-03-22 21:57:11 +0530

    Update on Storage

--- src/container/CropStocks/index.tsx
+++ src/container/CropStocks/index.tsx
@@ -1,19 +1,56 @@
 import React, {memo} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 import Container from 'layout/Container';
+import SearchBar from 'components/SearchBar';
+import TitleTextButton from 'components/TitleTextButton';
+import {Colors} from 'configs';
+import Text from 'components/Text';
+import Theme from 'style/Theme';
+import {IMAGE} from 'images';
 
 interface CropStocksProps {}
 
 const CropStocks = memo((_props: CropStocksProps) => {
   return (
     <Container>
-      <View style={styles.tempview} />
+      <TitleTextButton title="Storage" buttonText="Add Stocks" />
+      <SearchBar style={styles.search} placeholder="Search Storage" />
+      <View style={styles.overview}>
+        <Image source={IMAGE.box} style={styles.itemimage} />
+        <View style={styles.itemtext}>
+          <Text description>Total Products</Text>
+          <Text heading>30</Text>
+        </View>
+      </View>
+      <View style={styles.itembox}></View>
     </Container>
   );
 });
 
 const styles = StyleSheet.create({
-  tempview: {},
+  search: {
+    marginTop: 10,
+  },
+  overview: {
+    height: 100,
+    margin: 20,
+    backgroundColor: Colors.White,
+    borderRadius: 10,
+    ...Theme.flexRow,
+  },
+  itembox: {
+    height: 100,
+    padding: 10,
+    margin: 20,
+    backgroundColor: Colors.LightGray,
+  },
+  itemimage: {
+    height: 80,
+    width: 80,
+  },
+  itemtext: {
+    margin: 20,
+  },
 });
 
 export default CropStocks;
--- src/images/index.ts
+++ src/images/index.ts
@@ -2,4 +2,5 @@ export const IMAGE: any = {
   textLogo: require('images/name-solfi-logo.png'),
   serialprotoype: require('images/serialprototype.png'),
   hardware: require('images/hardware.png'),
+  box: require('images/box.png'),
 };
