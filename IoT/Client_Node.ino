7f3c21b6141a586b22ee53807c5ccfeb1ffbca7e
Belgin <Belgin.confidential@gmail.com>
2023-04-17 23:26:23 +0530

    Main

--- IoT/Client_Node.ino
+++ IoT/Client_Node.ino
@@ -1,30 +1,36 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid = "NodeMain";
-const char* password = "testing";
+const char *ssid = "NodeMain";
+const char *password = "testing";
 
-void setup() {
+void setup()
+{
   Serial.begin(115200); // Start serial communication
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to hotspot...");
   }
   Serial.println("Connected to hotspot");
 }
 
-void loop() {
+void loop()
+{
   WiFiClient client("192.168.4.1", 80);
-  if (client.connect()) {
+  if (client.connect())
+  {
     // Send a request to the server to get the sensor data
     client.println("GET /getSensorData HTTP/1.1");
     client.println("Host: 192.168.4.1");
     client.println("Connection: close");
     client.println();
 
-    while (client.connected()) {
+    while (client.connected())
+    {
       // Wait for data to be available
-      while (client.available() == 0) {
+      while (client.available() == 0)
+      {
         delay(100);
       }
 
--- IoT/Server.ino
+++ IoT/Server.ino
@@ -2,28 +2,30 @@
 #include <FirebaseESP8266.h>
 
 // Set the credentials for your WiFi network
-const char* ssid = "NodeMain";
-const char* password = "testing";
+const char *ssid = "NodeMain";
+const char *password = "testing";
 
 // Set the Firebase project's authentication credentials
 #define FIREBASE_HOST "solfi.firebaseio.com"
 #define FIREBASE_AUTH "AIzaSyBb-HcuoEwRvmjpcAyWLVwg32CQ49wKx-U"
 
 const firebaseConfig = {
-  authDomain: "sol-fi.firebaseapp.com",
-  projectId: "sol-fi",
-  storageBucket: "sol-fi.appspot.com",
-  messagingSenderId: "814353624",
-  appId: "1:814353624:web:91f03d452f996805b39bad",
-  measurementId: "G-NWTKC5Z06Z"
+  authDomain : "sol-fi.firebaseapp.com",
+  projectId : "sol-fi",
+  storageBucket : "sol-fi.appspot.com",
+  messagingSenderId : "814353624",
+  appId : "1:814353624:web:91f03d452f996805b39bad",
+  measurementId : "G-NWTKC5Z06Z"
 };
 
-void setup() {
-  pinMode(LED_PIN, OUTPUT); // Set the LED pin as output
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);   // Set the LED pin as output
   digitalWrite(LED_PIN, LOW); // Turn off the LED
-  Serial.begin(115200); // Start serial communication
+  Serial.begin(115200);       // Start serial communication
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to hotspot...");
   }
@@ -31,18 +33,22 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
-void loop() {
+void loop()
+{
   WiFiClient client("192.168.4.1", 80);
-  if (client.connect()) {
+  if (client.connect())
+  {
     // Send a request to the server to get the sensor data
     client.println("GET /getSensorData HTTP/1.1");
     client.println("Host: 192.168.4.1");
     client.println("Connection: close");
     client.println();
 
-    while (client.connected()) {
+    while (client.connected())
+    {
       // Wait for data to be available
-      while (client.available() == 0) {
+      while (client.available() == 0)
+      {
         delay(100);
       }
 
